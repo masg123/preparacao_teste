3e8905c17bd4c9ba4740d9dbd3e6c7d79c7c1f96
UserPL135Pc18 <UserPL135Pc18@SLFRPL135PC18.ensino.atec.local>
2016-12-20 14:19:40 +0000

    calculadora

--- src/dificuldade_3/exercicio_10.java
+++ src/dificuldade_3/exercicio_10.java
@@ -6,59 +6,58 @@ public class exercicio_10 {
 
 	public exercicio_10() {
 		
-		
-		int num1;
-		int num2;
-		int escolha = 0; 
-		
-		
-		
-		System.out.println(" Introduza o primeiro numero");
-		num1 =(new Scanner(System.in)).nextInt();
-		
-		
-		
-		
-		System.out.println(" -Qual a operação que deseja?");
-		System.out.println(" -Soma ");
-		System.out.println(" -Subtração ");
-		System.out.println(" -Dividir ");
-		System.out.println(" -Multiplicar ");
-		System.out.println(" -Sair ");
-		
-		System.out.println(" -escolha ");
-		
-		System.out.println(" Introduza o segundo numero");
-		num2 =(new Scanner(System.in)).nextInt();
-		
-		escolha = (new Scanner(System.in)).nextInt();
-		
-		
-			
-			if (escolha == 1){
-				
-				int soma = num1 + num2;
-				
-			}if( escolha == 2){
-				
-				int subtrair = num1 - num2;
-					
-			}if ( escolha == 3){
-				
-				int Divisao = num1 / num2;
-				
-			}if (escolha == 4){
-				
-				int multiplicação = num1*num2;
-				
-				
-			}else
-				System.out.println(" End ");
-			
-			
-		
-	
-	}
+		 Scanner reader = new Scanner(System.in);
+		    int num1,num2;
+		    char op;
+		    System.out.println("----Exercicio Calculadora Infinita----");
+		    System.out.print("Introduza um numero:");
+		    num1=reader.nextInt();
+		    
+		    do{
+		    System.out.println("Escolha uma operação: ");
+		    System.out.println("+ Soma");
+		    System.out.println("- Subtrair");
+		    System.out.println("* Multiplicar");
+		    System.out.println("/ Dividir");
+		    System.out.println("0 Sair do Programa");
+		    op=reader.next().charAt(0);
+		     
+		    if(op!='0'){
+		    
+		        System.out.print("Introduza um numero:");
+		        num2=reader.nextInt();
+		    
+		            switch(op){
+
+		                case '+':
+		                num1=num1+num2;
+		                System.out.println(num1);
+		                break;
+		                case '-':
+		                num1=num1-num2;
+		                System.out.println(num1);
+		                break;
+		                case '*':
+		                num1=num1*num2;
+		                System.out.println(num1);
+		                break;
+		                case '/':
+		                num1=num1/num2;
+		                System.out.println(num1);
+		                break;
+		                default:
+		                System.out.println("Operação invalida");
+
+
+		            }//Fim do switch
+		            
+		            
+		    }else if(op=='0')
+		        System.out.println("Fim do Programa Calculadora Infinita");
+		        
+		    }while(op!='0');
+		    
+		    }
 		
 	}
 
