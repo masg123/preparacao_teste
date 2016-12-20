11f5f60d45c2d6f96a1f83a8e85807cc9c80a046
UserPL135Pc18 <UserPL135Pc18@SLFRPL135PC18.ensino.atec.local>
2016-12-20 15:14:15 +0000

    exerccicio

---
+++ src/dificuldade_0/exercicio_4.java
@@ -0,0 +1,13 @@
+package dificuldade_0;
+
+public class exercicio_4 {
+	int[] n = new int[]{3,5,6,1};
+	int soma=0;
+	
+	for(int i=0; i<n.length;i++){
+		soma+=n[i];
+	}
+	
+	System.out.println("O valor da soma dos valores declarados no array é de: " + soma);
+}
+}
